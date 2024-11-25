8d13dd27ce7d0f507efefd7677eb0924a28265c9
Siraj Us Salekin <rupaksalekin2018@gmail.com>
2024-11-26 00:20:09 +0200

    Model trainer & tracking set up using Mlflow is done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from src.logger import logging
 from src.logger import logging
 from src.exception import CustomException
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
 
@@ -53,3 +54,6 @@ if __name__ == "__main__":
 
     data_transformation = DataTransformation()
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
